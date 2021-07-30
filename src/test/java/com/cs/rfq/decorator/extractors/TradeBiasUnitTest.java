2fb4ea8f0742c8ffebcf153d3b0dee79c04aaf5a
joe-natale <jrn84@rutgers.edu>
2021-07-30 17:16:16 -0400

    Added initial unit test for TradeBias extractor.
    Note: setSince function seems to not work.

--- src/test/java/com/cs/rfq/decorator/extractors/TradeBiasUnitTest.java
+++ src/test/java/com/cs/rfq/decorator/extractors/TradeBiasUnitTest.java
@@ -21,7 +21,7 @@ public class TradeBiasUnitTest extends AbstractSparkUnitTest{
         rfq.setEntityId(5561279226039690843L);
         rfq.setIsin("AT0000A0VRQ6");
 
-        String filePath = getClass().getResource("average-traded-1.json").getPath();
+        String filePath = getClass().getResource("trade-bias.json").getPath();
         String noMatchPath = getClass().getResource("volume-traded-1.json").getPath();
         String filePath2 = getClass().getResource("liquiditytest.json").getPath();
         trades = new TradeDataLoader().loadTrades(session, filePath);
@@ -30,40 +30,14 @@ public class TradeBiasUnitTest extends AbstractSparkUnitTest{
     }
 
     @Test
-    public void checkVolumeWhenAllTradesMatch() {
-
-        InstrumentLiquidityExtractor extractor = new InstrumentLiquidityExtractor();
+    public void checkPositiveTradeBiasPercentage(){
+        TradeBiasExtractor extractor = new TradeBiasExtractor();
         extractor.setSince("2021-07-30");
 
         Map<RfqMetadataFieldNames, Object> meta = extractor.extractMetaData(rfq, session, trades);
 
-        Object months = meta.get(RfqMetadataFieldNames.liquidityVolume);
+        Object monthBias = meta.get(RfqMetadataFieldNames.tradeBiasBuyPercentagePastMonth);
 
-        assertEquals(1_350_000L, months);
-    }
-
-    @Test
-    public void getVolumesForDifferentsMonthsWeeksYears() {
-
-        InstrumentLiquidityExtractor extractor = new InstrumentLiquidityExtractor();
-        extractor.setSince("2021-07-30");
-
-        Map<RfqMetadataFieldNames, Object> meta = extractor.extractMetaData(rfq, session, trades2);
-
-        Object months = meta.get(RfqMetadataFieldNames.liquidityVolume);
-
-        assertEquals(950_000L, months);
-    }
-
-    @Test
-    public void checkNoMatchesonTrades() {
-        InstrumentLiquidityExtractor extractor = new InstrumentLiquidityExtractor();
-        extractor.setSince("2021-07-30");
-
-        Map<RfqMetadataFieldNames, Object> meta = extractor.extractMetaData(rfq, session, noMatches);
-
-        Object result = meta.get(RfqMetadataFieldNames.liquidityVolume);
-
-        assertEquals(0, (int) result);
+        assertEquals(40, monthBias);
     }
 }
